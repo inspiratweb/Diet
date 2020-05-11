96075a56bbb2db855de49c789a09a384afacff97
Francisco Sanchez <francisco.sanchez@cabify.com>
2020-05-11 14:28:51 +0200

    better .env management

--- src/firebase.js
+++ src/firebase.js
@@ -3,10 +3,10 @@ import 'firebase/database';
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  authDomain: `${process.env.REACT_APP_PROJECT_ID}.firebaseapp.com`,
+  databaseURL: `https://${process.env.REACT_APP_PROJECT_ID}.firebaseio.com`,
   projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  storageBucket: `${process.env.REACT_APP_PROJECT_ID}.appspot.com`,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appID: process.env.REACT_APP_APP_ID
 };
